92b4a6aa8365d8ae0f945d2f7f6b7c1f86fd2e8f
missChewbacka <alexandra@evolany.com>
2024-02-06 13:47:43 +0200

    test without .bat

--- setup.bat
+++ setup.bat
@@ -9,8 +9,4 @@ pip install -r requirement.txt
 :: Add virtual environment's Scripts folder to PATH
 set PATH=%PATH%;C:\Users\Professional\PycharmProjects\RobotFramework\venv\Scripts
 
-:: Download and manage browser drivers
-C:\Users\Professional\PycharmProjects\RobotFramework\venv\Scripts\webdriver-manager update --versions.chrome=<chrome_version> --standalone
-C:\Users\Professional\PycharmProjects\RobotFramework\venv\Scripts\webdriver-manager update --versions.firefox=<firefox_version> --standalone
-
 echo Setup complete.
